57bd781b4bd9e75f7c90e5cc1a7cffa063e6ee7a
MatíasJMaranga <marangamatias@gmail.com>
2023-09-30 10:19:43 -0300

    debugging_4

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), 
-    Unfonts({
-      google: {
-        families: [{
-          name: 'Public Sans',
-          styles: 'wght@300;400;700',
-          defer: true
-        }],
-      },
-    }),
-  ],
-  base: "/carfixsolutions"
+  plugins: [react()],
 })
+
